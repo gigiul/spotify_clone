e291f6bf05e742cbd3b2e6f1465d3ca14990faf0
gigiul <luigidalleaste@gmail.com>
2023-01-30 12:28:01 +0100

    implemented Relatedd Song and Artist Details but for the latter I don't have a valid api key

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,18 +5,16 @@ import PlayPause from './PlayPause';
 import {playPause, setActiveSong} from '../redux/features/playerSlice'
 
 const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
-
-const dispatch = useDispatch();
-const handlePauseClick = () => {
-  dispatch(playPause(false));
+  const dispatch = useDispatch();
   
-}
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  }
 
-const handlePlayClick = () => {
-  dispatch(setActiveSong({song, data, i}));
-   dispatch(playPause(true));
-
-}
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({song, data, i}));
+    dispatch(playPause(true));
+  }
 
 return (
   <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
