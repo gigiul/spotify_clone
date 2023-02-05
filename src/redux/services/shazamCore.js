c198c0b5166e7fc74aaa2a14cdc0547dbccf5732
gigiul <luigidalleaste@gmail.com>
2023-02-05 16:17:35 +0100

    Implemented other section but I had a limited API key so don't work properly

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,6 +14,9 @@ endpoints: (builder) => ({
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),  
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
 }),
 });
 
@@ -22,4 +25,7 @@ export const {
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsBySearchQuery,
 } = shazamCoreApi;
